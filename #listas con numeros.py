62bd9c790442ec1e2217826020ef4ae0c4a50e76
roblescentenobianca-art <roblescentenobianca@gmail.com>
2026-04-21 18:43:54 -0600

    Add files via upload

---
+++ #listas con numeros.py
@@ -0,0 +1,13 @@
+#lista con numeros
+#centeno ximena
+numeros = [45, 12, 17, 78, 3, 56, 89, 23, 67, 1, 34]
+print('Original:',numeros)
+
+#Ordenar de menor a mayor
+numeros.sort()
+print('Ordenada:' ,numeros)
+
+#Ordenar de mayor a menor
+
+numero.reverse()
+print('Invertida:', numeros)
