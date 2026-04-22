f20b683cdfb504b462d3451de25ecaef6e80a40b
roblescentenobianca-art <roblescentenobianca@gmail.com>
2026-04-21 18:44:09 -0600

    Add files via upload

---
+++ #Operacion con listas.py
@@ -0,0 +1,13 @@
+#Operacion con listas
+#centeno ximena
+
+frutas = ['manzana' , 'pera' , 'uva', 'mango']
+
+print(frutas[0])
+print(frutas[-1])
+
+frutas[1] = 'naranja'
+frutas.append('kiwi')
+frutas.insert(1 , 'fresa')
+frutas.remove('uva')
+frutas.pop(0)
