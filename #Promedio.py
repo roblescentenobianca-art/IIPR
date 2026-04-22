5c551bcf832eb93271875a84c2a6b1634ed2f888
roblescentenobianca-art <roblescentenobianca@gmail.com>
2026-04-21 18:44:49 -0600

    Add files via upload

---
+++ #Promedio.py
@@ -0,0 +1,8 @@
+#Promedio
+#centeno ximena 
+numeros = [45, 12, 17, 78, 3, 56, 89, 23, 67, 1, 34]
+
+total = sum(numeros)
+promedio = sum(numeros)  / len(numeros)
+print(f'suma:{total}')
+print(f'Promedio: {promedio:.2f}')
