a690ee474517ade13ac235a74e4873c60681deea
roblescentenobianca-art <roblescentenobianca@gmail.com>
2026-04-21 18:45:07 -0600

    Add files via upload

---
+++ #Sorteo.py
@@ -0,0 +1,14 @@
+#Sorteo
+#centeno ximena
+
+original =[45, 12, 78, 3, 56]
+nueva = sorted(original)
+desc = sorted(original, reverse=True)
+
+print("Lista de participantes:", original)
+
+print('ascendente:', original)
+print('descendente:', original)
+      
+print('original:', original)
+print('Ganador sorteo:', desc[0])
