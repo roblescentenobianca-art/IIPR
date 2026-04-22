26e5c2886646cd19bf7cbaebc599150d8af12d91
roblescentenobianca-art <roblescentenobianca@gmail.com>
2026-04-21 18:43:39 -0600

    Add files via upload

---
+++ #Calificaciones y promedios.py
@@ -0,0 +1,18 @@
+#Calificaciones y promedios
+#centeno ximena
+
+cal1 = 8
+cal2 = 9
+cal3 = 7 
+cal4 = 10
+cal5 = 8
+
+#Calculamos promedio
+promedio = (cal1 + cal2 + cal3 + cal4 + cal5) / 5
+print("Las calificaciones son:", cal1, cal2, cal3, cal4, cal5)
+print("El promedio es:", promedio)
+      
+
+
+
+
